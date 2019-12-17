abe63c107e532b89087b35d5cdf9e2d5203e7fd3
root <root@275319.local>
2019-12-17 14:32:33 +0300

    add git

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :issue_rating do
+  name 'Issue Rating plugin'
+  author 'MIIIG'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://github.com/path/to/plugin'
+  author_url 'http://github.com/MIIIG'
+end
